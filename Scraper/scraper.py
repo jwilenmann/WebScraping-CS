0b389986f1646bccc7908986c9a4d96c56d3ad48
jwilenmann <javier.wilenmann@uai.cl>
2022-09-01 09:47:01 -0400

    nuevo

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -28,6 +28,28 @@ def select_option(driver, xpath, index):
     select_object = Select(driver.find_element(By.XPATH, xpath))
     select_object.select_by_value(index)
 
+def getDownLoadedFileName(waitTime):
+    driver.execute_script("window.open()")
+    # switch to new tab
+    driver.switch_to.window(driver.window_handles[-1])
+    # navigate to chrome downloads
+    driver.get('chrome://downloads')
+    # define the endTime
+    endTime = time.time()+waitTime
+    while True:
+        try:
+            # get downloaded percentage
+            downloadPercentage = driver.execute_script(
+                "return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value")
+            # check if downloadPercentage is 100 (otherwise the script will keep waiting)
+            if downloadPercentage == 100:
+                # return the file name once the download is completed
+                return driver.execute_script("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content  #file-link').text")
+        except:
+            pass
+        time.sleep(1)
+        if time.time() > endTime:
+            break
 
 url = 'https://www.pjud.cl/tribunales/corte-suprema'
 chrome_options = Options()
@@ -43,49 +65,65 @@ driver.implicitly_wait(10)
 #anos = ["2015", "2016", "2017", "2018", "2019"]
 anos = ["2022"] 
 meses = ["08"]
-
+c_ano = "2022"
+c_mes = "08"
 Integraciones = driver.find_element(By.XPATH, 
 "/html/body/section/div/div/div[1]/div[2]/div/div/div/div/div/div/div/div[1]/div[2]/div[1]/div/div/div[3]/div/div/a[6]")
 Integraciones.click()
 
-for ano in anos:
+#for ano in anos:
     # Estos indices se obtienen de las opciones de la pagina, cada uno es un mes, cambiar para obtener mas meses
     #for index in range(1, 2):
         # Se envia la info a la pagina /html/body/div[11]/div/div/div[2]/form/div[2]/div[1]/div/select
-    select_option(driver, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[1]/div/select', str(ano))
-    for mes in meses:
-        select_option(driver, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[2]/div/select', mes)
-        select = Select(driver.find_element(By.XPATH, "/html/body/div[11]/div/div/div[2]/form/div[2]/div[3]/div/select")) #get all the options into a list
-        optionsList = []
-        for item in select.options:
-            optionsList.append(item.get_attribute("value"))            
-            for optionValue in optionsList:
-                select_option(driver, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[3]/div/select', optionValue)
+select_option(driver, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[1]/div/select', c_ano)
+    #for mes in meses:
+select_option(driver, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[2]/div/select', c_mes)
+select = Select(driver.find_element(By.XPATH, "/html/body/div[11]/div/div/div[2]/form/div[2]/div[3]/div/select")) #get all the options into a list
+optionsList = []
+for item in select.options:
+    optionsList.append(item.get_attribute("value"))            
+    for optionValue in optionsList:
+        select_option(driver, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[3]/div/select', optionValue)
 
         # Se hace click con los datos llenados
-                driver.find_element(By.XPATH, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[4]/button').click()
+        driver.find_element(By.XPATH, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[4]/button').click()
         
         # Se descargan los datos
-                wait = WebDriverWait(driver, 10) 
+        wait = WebDriverWait(driver, 10) 
     #descarga por salas cambia último tr: 1, 2, 3, 4 - no siempre está disponible, el mismo día, las mismas salas
-                try: 
-                    boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[1]/td[1]/a')))
-                    boton_descarga.click()
-                except: 
-                    pass
-                try: 
-                    boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[2]/td[1]/a')))
-                    boton_descarga.click()
-                except: 
-                    pass
-                try: 
-                    boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[3]/td[1]/a')))
-                    boton_descarga.click()
-                except: 
-                    pass
-                try: 
-                    boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[4]/td[1]/a')))
-                    boton_descarga.click()
-                except: 
-                    pass
+        try: 
+            boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[1]/td[1]/a')))
+            boton_descarga.click()
+            latestDownloadedFileName = getDownLoadedFileName(10) #waiting 3 minutes to complete the download
+            print(latestDownloadedFileName)
+            
+        except: 
+            pass
+        
+        try: 
+            boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[2]/td[1]/a')))
+            boton_descarga.click()
+            latestDownloadedFileName = getDownLoadedFileName(10) #waiting 3 minutes to complete the download
+            print(latestDownloadedFileName) 
+
+        except: 
+            pass
+        
+        try: 
+            boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[3]/td[1]/a')))
+            boton_descarga.click()
+            latestDownloadedFileName = getDownLoadedFileName(10) #waiting 3 minutes to complete the download
+            print(latestDownloadedFileName)
+
+        except: 
+            pass
+        
+        try: 
+            boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[4]/td[1]/a')))
+            boton_descarga.click()
+            latestDownloadedFileName = getDownLoadedFileName(10) #waiting 3 minutes to complete the download
+            print(latestDownloadedFileName)
+
+        except: 
+            pass
 driver.close
