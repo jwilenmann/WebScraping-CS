5ac09b289900028675f4aa0ea5daa2e1bd019cb3
jwilenmann <javier.wilenmann@uai.cl>
2022-09-01 18:20:56 -0400

    m

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -29,13 +29,17 @@ def select_option(driver, xpath, index):
     select_object.select_by_value(index)
 
 
+
 url = 'https://www.pjud.cl/tribunales/corte-suprema'
 chrome_options = Options()
 path = os.path.join(os.getcwd(), "output")
-prefs = {"download.default_directory" : path,  "download.prompt_for_download": False,
-    "download.directory_upgrade": True}
+prefs = {"download.default_directory" : path,  "directory_upgrade": True}
 chrome_options.add_experimental_option("prefs",prefs)
-chrome_options.add_argument('--headless')
+Sala1 = path + "\\" + "Sala+1.pdf"
+Sala2 = path + "\\" + "Sala+2.pdf"
+Sala3 = path + "\\" + "Sala+3.pdf"
+Sala4 = path + "\\" + "Sala+4.pdf"
+#chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options = chrome_options)
 driver.get(url)
 
@@ -60,23 +64,6 @@ select_option(driver, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[2]/div/
 select = Select(driver.find_element(By.XPATH, "/html/body/div[11]/div/div/div[2]/form/div[2]/div[3]/div/select")) #get all the options into a list
 optionsList = []
 
-
-
-counter1 = 0
-counter2 = 0
-counter3 = 0
-counter4 = 0
-
-old_name1 = path + "\\" + "Sala+1.pdf"
-old_name2 = path + "\\" + "Sala+2.pdf"
-old_name3 = path + "\\" + "Sala+3.pdf"
-old_name4 = path + "\\" + "Sala+4.pdf"
-
-new_name1 = old_name1 
-new_name2 = old_name2 
-new_name3 = old_name3 
-new_name4 = old_name4 
-
 for item in select.options:
     optionsList.append(item.get_attribute("value"))            
     for optionValue in optionsList:
@@ -86,30 +73,64 @@ for item in select.options:
         driver.find_element(By.XPATH, '/html/body/div[11]/div/div/div[2]/form/div[2]/div[4]/button').click()
         
         # Se descargan los datos
-        wait = WebDriverWait(driver, 5) 
+        wait = WebDriverWait(driver, 3) 
     #descarga por salas cambia último tr: 1, 2, 3, 4 - no siempre está disponible, el mismo día, las mismas salas
         try: 
             boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[1]/td[1]/a')))
             boton_descarga.click()
-            os.rename(old_name1, new_name1)
-            old_name1 = new_name1
-            counter1 += 1
-            new_name1 = new_name1[:-4] + "_" + str(counter1) + ".pdf"
-
-
-
-
+            pdfFileObj = open(Sala1,'rb')               
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)   
+            num_pages = pdfReader.numPages
+            pdfPageObj = pdfReader.getPage(0)
+            text = pdfPageObj.extractText()
+            Causa = []
+            if re.search("INSTALACI", text):
+                Integrantes = re.findall('(?<=ÑOR).*?(?=\s)', text)
+                try:
+                    Fecha = re.findall('(?<=FIRMADIGITAL).*?(?=\\\n)', text)[0]
+                except:
+                    pass
+                try:
+                    Sala = re.findall('(?<=\\s).*?(?=\\sSALA)', text)[0]
+                except:
+                    pass
+                for palabra in palabras:         
+                    if palabra in text:
+                        Causa.append(palabra)
+                df = df.append(
+               {"Fecha" : str(Fecha), "Sala" : str(Sala), "Integrantes" : Integrantes, 'Causa' : Causa},
+               ignore_index=True,
+            )
+            
         except: 
             pass
         
         try: 
             boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[2]/td[1]/a')))
             boton_descarga.click()
-            os.rename(old_name2, new_name2)
-            old_name2 = new_name2
-            counter2 += 1
-            new_name2 = new_name2[:-4] + "_" + str(counter2) + ".pdf"
-
+            pdfFileObj = open(Sala2,'rb')               
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)   
+            num_pages = pdfReader.numPages
+            pdfPageObj = pdfReader.getPage(0)
+            text = pdfPageObj.extractText()
+            Causa = []
+            if re.search("INSTALACI", text):
+                Integrantes = re.findall('(?<=ÑOR).*?(?=\s)', text)
+                try:
+                    Fecha = re.findall('(?<=FIRMADIGITAL).*?(?=\\\n)', text)[0]
+                except:
+                    pass
+                try:
+                    Sala = re.findall('(?<=\\s).*?(?=\\sSALA)', text)[0]
+                except:
+                    pass
+                for palabra in palabras:         
+                    if palabra in text:
+                        Causa.append(palabra)
+                df = df.append(
+               {"Fecha" : str(Fecha), "Sala" : str(Sala), "Integrantes" : Integrantes, 'Causa' : Causa},
+               ignore_index=True,
+            )
 
         except: 
             pass
@@ -117,10 +138,30 @@ for item in select.options:
         try: 
             boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[3]/td[1]/a')))
             boton_descarga.click()
-            os.rename(old_name3, new_name3)
-            old_name3 = new_name3
-            counter3 += 1
-            new_name3 = new_name3[:-4] + "_" + str(counter3) + ".pdf"
+            pdfFileObj = open(Sala3,'rb')               
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)   
+            num_pages = pdfReader.numPages
+            pdfPageObj = pdfReader.getPage(0)
+            text = pdfPageObj.extractText()
+            Causa = []
+            if re.search("INSTALACI", text):
+                Integrantes = re.findall('(?<=ÑOR).*?(?=\s)', text)
+                try:
+                    Fecha = re.findall('(?<=FIRMADIGITAL).*?(?=\\\n)', text)[0]
+                except:
+                    pass
+                try:
+                    Sala = re.findall('(?<=\\s).*?(?=\\sSALA)', text)[0]
+                except:
+                    pass
+                for palabra in palabras:         
+                    if palabra in text:
+                        Causa.append(palabra)
+                df = df.append(
+               {"Fecha" : str(Fecha), "Sala" : str(Sala), "Integrantes" : Integrantes, 'Causa' : Causa},
+               ignore_index=True,
+            )
+
 
         except: 
             pass
@@ -128,10 +169,30 @@ for item in select.options:
         try: 
             boton_descarga = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[11]/div/div/div[2]/table/tbody/tr[4]/td[1]/a')))
             boton_descarga.click()
-            os.rename(old_name4, new_name4)
-            old_name4 = new_name4
-            counter4 += 1
-            new_name4 = new_name4[:-4] + "_" + str(counter4) + ".pdf"
+            pdfFileObj = open(Sala1,'rb')               
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)   
+            num_pages = pdfReader.numPages
+            pdfPageObj = pdfReader.getPage(0)
+            text = pdfPageObj.extractText()
+            Causa = []
+            if re.search("INSTALACI", text):
+                Integrantes = re.findall('(?<=ÑOR).*?(?=\s)', text)
+                try:
+                    Fecha = re.findall('(?<=FIRMADIGITAL).*?(?=\\\n)', text)[0]
+                except:
+                    pass
+                try:
+                    Sala = re.findall('(?<=\\s).*?(?=\\sSALA)', text)[0]
+                except:
+                    pass
+                for palabra in palabras:         
+                    if palabra in text:
+                        Causa.append(palabra)
+                df = df.append(
+               {"Fecha" : str(Fecha), "Sala" : str(Sala), "Integrantes" : Integrantes, 'Causa' : Causa},
+               ignore_index=True,
+            )
+
 
         except: 
             pass
